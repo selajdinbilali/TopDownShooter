8096de403ad34dc7fac6b196a4c4f655de0977f8
Selajdin Bilali <selajdin.bilali@gmail.com>
2017-10-10 16:58:33 +0200

    Episode 01 done, my comments added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,35 +4,40 @@ using UnityEngine;
 
 [RequireComponent (typeof (PlayerController))]
 public class Player : MonoBehaviour {
+    
     public float moveSpeed = 5f;
 
-
+    // we need a ref to the camera for the ray cast
     Camera viewCamera;
     PlayerController controller;
 
-	// Use this for initialization
 	void Start () {
         controller = GetComponent<PlayerController>();
         viewCamera = Camera.main;
 	}
 	
-	// Update is called once per frame
 	void Update () {
         // Move input
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        // we call the Move method of the PlayerController class
         controller.Move(moveVelocity);
 
         // fire a ray from the camera on the ground
         Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
         // instersect with ground
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        // this var will be filled by the Raycast method
         float rayDistance;
 
+        // if the ground interesects the ray
         if (groundPlane.Raycast(ray, out rayDistance))
         {
+            // we get the point of intersection
             Vector3 point = ray.GetPoint(rayDistance);
             //Debug.DrawLine(ray.origin, point, Color.red);
+
+            // we call the LookAt method of the PlayerController
             controller.LookAt(point);
         }
 	}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,32 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// the playercontroller requires a rigidbody
+// this line will add it automatically in unity editor
 [RequireComponent (typeof (Rigidbody))]
-
 public class PlayerController : MonoBehaviour {
 
+    // private variables
     Vector3 velocity;
     Rigidbody myRigidbody;
-
 	
 	void Start ()
     {
+        // we have to get the rigidbody
         myRigidbody = GetComponent<Rigidbody>();
 	}
 
     public void Move(Vector3 vel)
     {
+        // reset the velocity, this method is called from the Player class
         velocity = vel;
     }
 
     public void LookAt(Vector3 lookPoint)
     {
+        // corrects the height because the player will tilt to look at the ground
         Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        // make the player look at the point
         transform.LookAt(heightCorrectedPoint);
     }
 
     private void FixedUpdate()
     {
+        // we always move rigidbodies in fixedupdate
         myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
     }
 
