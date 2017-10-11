317bbf2906c90c7e5674ab643e02ab6469209d31
Selajdin Bilali <selajdin.bilali@gmail.com>
2017-10-11 09:29:47 +0200

    Episode 03 done, my comments added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// add this to use the navmeshagent
+using UnityEngine.AI;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class Enemy : MonoBehaviour {
+
+    NavMeshAgent pathFinder;
+    Transform target;
+
+	
+	void Start () {
+        pathFinder = GetComponent<NavMeshAgent>();
+        // the enemy will chase the player
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
+	}
+	
+	
+	void Update () {
+        
+	}
+
+    // to avoid calculating the path every frame
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = 0.25f;
+        // while there is a target
+        while (target != null)
+        {
+            // go chase the player
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathFinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-    
+    // the collision mask is important to not calculate collision with everything
+    public LayerMask collisionMask;
     float speed = 10f;
 
+    // set the speed of the projectile for example if we have different weapons
     public void SetSpeed(float newSpeed)
     {
         speed = newSpeed;
     }
 
 	void Update () {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        float moveDistance = speed * Time.deltaTime;
+        // check if the projectile collides with an enemy
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
 	}
+
+    void CheckCollisions(float moveDistance)
+    {
+        // the ray starts from the projectil and goes forward
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        // if we have a hit in our collission layer
+        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+
+    }
+
+    void OnHitObject(RaycastHit hit)
+    {
+        // we print the name of the object hit
+        Debug.Log(hit.collider.gameObject.name);
+        // we destroy the projectile
+        GameObject.Destroy(gameObject);
+    }
 }
